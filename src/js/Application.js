ef4200caeb5a803cdcd60795f002b998a41caf2b
ivaylo91 <ipenev91@gmail.com>
2023-01-01 18:15:09 +0200

    add emojis

--- src/js/Application.js
+++ src/js/Application.js
@@ -25,6 +25,7 @@ export default class Application extends EventEmitter {
 
     addBananas() {
         const feed = this.emojis.map(m => m + this.banana);
+        this.emojis = feed;
         this.setEmojis(feed);
     }
 }
