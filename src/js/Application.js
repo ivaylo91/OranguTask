d1f27bc6debc4251e88395c869825c8137f68559
ivaylo91 <ipenev91@gmail.com>
2023-01-01 18:08:19 +0200

    add emojis

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,21 +1,30 @@
 import EventEmitter from "eventemitter3";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
-    this.emojis = [];
-    this.banana = "🍌";
-    this.emit(Application.events.READY);
-  }
-  setEmojis(emojis) {
-    this.emojis = emojis;
-  }
+    constructor() {
+        super();
+        this.emojis = [];
+        this.banana = "🍌";
+        this.emit(Application.events.READY);
+    }
 
-  addBananas() {}
+    setEmojis(emojis) {
+        const emojiDiv = document.querySelector("#emojis");
+        emojiDiv.textContent = "";
+        this.emojis = emojis;
+        let emojiP = document.createElement("p");
+        emojiP.textContent = this.emojis.join(" ");
+        emojiDiv.appendChild(emojiP);
+    }
+
+    addBananas() {
+        const feed = this.emojis.map(m => m + this.banana);
+        this.setEmojis(feed);
+    }
 }
